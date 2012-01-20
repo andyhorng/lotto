87a0aedc088a76bbabd5b1d2ef2a501423c8e471
洪盟翔 <andyhorng168@gmail.com>
2012-01-21 01:39:57 +0800

    init commit

---
+++ lotto.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+require 'colorize'
+
+class LuckyNumber
+
+  attr_reader :nums
+
+  def initialize
+    begin
+      @nums = gets.split.uniq
+    end until valid?
+    @nums = @nums.map {|num| num.to_i}.sort
+  end
+
+  def valid?
+    return true if @nums.empty?
+    return false if @nums.length != 6 
+    @nums.detect { |n| !n =~ /\d+/ } and return false
+    @nums.detect { |n| n.to_i > 49 } and return false
+    return true
+  end
+
+  def vs(lotto_number)
+    enum = @nums.each
+    lotto_enum = lotto_number.nums.each
+
+    result = []
+    loop do
+      begin
+        if enum.peek == lotto_enum.peek
+          result << enum.next.to_s.red
+          lotto_enum.next
+        elsif enum.peek > lotto_enum.peek
+          lotto_enum.next
+        else
+          result << enum.next.to_s
+        end
+        # rest
+      rescue StopIteration
+        result.concat(rest_of(enum))
+        puts result.join(", ") 
+        break
+      end
+    end
+  end
+
+  def rest_of(enum)
+    rest = []
+    loop do
+      begin
+        rest << enum.next
+      rescue StopIteration
+        return rest
+      end
+    end
+  end
+
+end
+
+
+class Main
+  def initialize
+    @lucky_numbers = []
+    i = 0 
+    loop do
+      i += 1
+      puts "lucky number #{i}:"
+      lucky_number = LuckyNumber.new
+      break if lucky_number.nums.empty?
+      @lucky_numbers << lucky_number
+    end
+
+    puts "lotto number: "
+    @lotto_number = LuckyNumber.new
+
+    # overview
+    puts "your lucky numbers:"
+    @lucky_numbers.each do |lucky_number|
+      puts lucky_number.nums.to_s
+    end
+    puts "your lotto numbers:"
+    puts @lotto_number.nums.to_s
+
+  end
+
+  def go
+    @lucky_numbers.each do |lucky_number|
+      lucky_number.vs(@lotto_number)
+    end
+  end
+end
+
+
+main = Main.new
+main.go
